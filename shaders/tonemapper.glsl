913cec4518c4d22a33fd0a6afb730ef130fb5cfc
Alex Mourtziapis <alex.mourtziapis@gmail.com>
2019-10-19 18:51:58 +0300

    Update tonemapper.glsl

--- shaders/tonemapper.glsl
+++ shaders/tonemapper.glsl
@@ -5,42 +5,61 @@ out vec4 FragColor;
 uniform sampler2D u_tex;
 uniform vec2 u_resolution;
 
-#define FILMIC
+// linear white point
+const float W = 1.2;
+const float T2 = 7.5;
 
-vec3 filmicToneMapping(vec3 color){
-	color = max(vec3(0.), color - vec3(0.004));
-	color = (color * (6.2 * color + .5)) / (color * (6.2 * color + 1.7) + 0.06);
-	return color;
+float filmic_reinhard_curve(float x) {
+	float q = (T2 * T2 + 1.0) * x * x;
+	return q / (q + x + T2 * T2);
+}
+
+vec3 filmic_reinhard(vec3 x) {
+	float w = filmic_reinhard_curve(W);
+	return vec3(
+		filmic_reinhard_curve(x.r),
+		filmic_reinhard_curve(x.g),
+		filmic_reinhard_curve(x.b)) / w;
+}
+
+const int N = 8;
+vec3 ca(sampler2D t, vec2 UV, vec4 sampl) {
+	vec2 uv = 1.0 - 2.0 * UV;
+	vec3 c = vec3(0);
+	float rf = 1.0;
+	float gf = 1.0;
+	float bf = 1.0;
+	float f = 1.0 / float(N);
+	for (int i = 0; i < N; ++i) {
+		c.r += f * texture(t, 0.5 - 0.5 * (uv * rf)).r;
+		c.g += f * texture(t, 0.5 - 0.5 * (uv * gf)).g;
+		c.b += f * texture(t, 0.5 - 0.5 * (uv * bf)).b;
+		rf *= 0.9972;
+		gf *= 0.998;
+		bf /= 0.9988;
+		c = clamp(c, 0.0, 1.0);
+	}
+	return c;
 }
 
 #define col tex.rgb
 
 void main()
 {
-	vec2 uv = gl_FragCoord.xy / u_resolution;
+	const float brightness = 1.0;
+	vec2 pp = gl_FragCoord.xy / u_resolution.xy;
+	vec2 p = 1. - 2. * gl_FragCoord.xy / u_resolution.xy;
 
-	vec4 tex = texture(u_tex, uv);
+	vec3 color = texture(u_tex, pp).rgb;
 
-#ifndef FILMIC
+	float vignette = 1.25 / (1.1 + 1.1 * dot(p, p));
+	vignette *= vignette;
+	vignette = mix(1.0, smoothstep(0.1, 1.1, vignette), 0.25);
+	color = color * vignette;
+	color = filmic_reinhard(brightness * color);
 
-	// gamma correction
-	const vec3 gamma = vec3(1. / 2.2);
-	// image exposure
-	const float exposure = 0.5;
+	color = smoothstep(-0.025, 1.0, color);
 
-#if 0
-	// exposure tone mapping
-	col = vec3(1.0) - exp(-col * exposure);
-#else
-	// reinhard tone mapping
-	col = col / (col + 1.0);
-#endif
-	// gamma correction
-	col = pow(col, gamma);
-
-#else
-	col = filmicToneMapping(col);
-#endif
-
-	FragColor = tex;
+	color = pow(color, vec3(1.0 / 2.2));
+	FragColor = vec4(color, 1.0);
 }

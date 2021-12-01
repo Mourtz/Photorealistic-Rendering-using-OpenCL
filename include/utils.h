01bf41f7d0e4fbeae5e71d6d80b504f252d8e57b
Alex <alex.mourtziapis@gmail.com>
2021-12-01 17:21:57 +0100

    Fix string exception for VC++ compiler

--- include/utils.h
+++ include/utils.h
@@ -1,23 +1,17 @@
 #pragma once
-
+#include <string>       // std::string
+#include <iostream>     // std::cout
 #include <fstream>
-#include <string>
+#include <sstream>      // std::stringstream
 
 namespace utils {
 
 	inline std::string ReadFile(std::string filepath)
 	{
 		std::ifstream t(filepath);
-		std::string str;
-
-		t.seekg(0, std::ios::end);
-		str.reserve(t.tellg());
-		t.seekg(0, std::ios::beg);
-
-		str.assign((std::istreambuf_iterator<char>(t)),
-				std::istreambuf_iterator<char>());
-
-		return str;
+		std::stringstream buffer;
+		buffer << t.rdbuf();
+		return buffer.str();
 	}
 
 	/**
